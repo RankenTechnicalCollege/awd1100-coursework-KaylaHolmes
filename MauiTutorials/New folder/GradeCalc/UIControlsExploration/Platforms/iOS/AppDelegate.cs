c7f59103bac29a6bf36ea0568469094b1513fd39
KaylaHolmes <kayla.holmes.0128@gmail.com>
2025-05-26 00:48:35 -0500

    MauiTutorial Completed

---
+++ MauiTutorials/New folder/GradeCalc/UIControlsExploration/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UIControlsExploration
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
